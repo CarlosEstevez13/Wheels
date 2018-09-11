17a01fae480d2b59ddd8df31cfc884749772ab3b
Carlos Estevez <caer09@gmail.com>
2018-09-11 11:57:03 -0500

    chat y viajes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,62 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  viajes= [
+    {
+      destino: 'UIS',
+      partida: 'San Francisco',
+      fecha: '14 de sep',
+      imagen: 'marty.jpeg',
+      nombre: 'Marty Mcfly',
+      hora: '03:00pm',
+      cupos: '3',
+    },
+    {
+      destino: 'UIS',
+      partida: 'Cabecera',
+      fecha: '14 de sep',
+      imagen: 'marty.jpeg',
+      nombre: 'Forest Gump',
+      hora: '02:00pm',
+      cupos: '2',
+    },
+    {
+      destino: 'UIS',
+      partida: 'Piedecuesta',
+      fecha: '15 de sep',
+      imagen: 'marty.jpeg',
+      nombre: 'Jenny Scatch',
+      hora: '01:00pm',
+      cupos: '3',
+    }
+  ];
 
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+
+  }
+
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirmacion de Cupo',
+      message: 'Desear unirte a este viaje?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar clickeado');
+          }
+        },
+        {
+          text: 'Unirse',
+          handler: () => {
+            console.log('Unirse clickeado');
+            this.navCtrl.setRoot(ContactPage);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
