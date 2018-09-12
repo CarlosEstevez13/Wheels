abc094f1ab12d9f9f660c200c9919b293fe73ade
Joao <joao.maldonado96@gmail.com>
2018-09-12 08:35:56 -0500

    cambioss

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-
-import { NgModule } from '@angular/core';
-import { IonicPageModule} from 'ionic-angular';
-=======
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
->>>>>>> e0c21405746172bb1363eb5f8669018b69531aef
 import { LoginPage } from './login';
 
 @NgModule({
