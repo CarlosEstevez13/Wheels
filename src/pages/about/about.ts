b326cd03fd0753a040c966aaf3262ac3f421730d
Joao <joao.maldonado96@gmail.com>
2018-09-12 18:52:23 -0500

    foto

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  image: string =null;
+  valido=false;
+  constructor(public navCtrl: NavController, private camera:Camera) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  getPicture(){
+    let options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      quality: 100
+    }
+    this.camera.getPicture( options )
+    .then(imageData => {
+      this.valido=true;
+      this.image = `data:image/jpeg;base64,${imageData}`;
+      
+    })
+    .catch(error =>{
+      console.error( error );
+    });
   }
 
 }
